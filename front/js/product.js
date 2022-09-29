e16ddb783720804d6305e8361617c4ceea91b74b
RomainNozay <romain.nozay@gmail.com>
2022-09-29 07:41:15 +0200

    Ajout prix et description page produit

--- front/js/product.js
+++ front/js/product.js
@@ -24,11 +24,17 @@ let informationUnObjet = fetch(`http://localhost:3000/api/products/${id}`)
 const positionImage = document.querySelector(".item__img ");
 console.log(positionImage);
 const positionTitre = document.querySelector("#title");
+const positionPrix = document.querySelector("#price");
+const positionDescription = document.querySelector("#description");
 
 const structureImage = `<img src=${data.imageUrl}> `;
 const structureTitre = `<h1>${data.name}</h1>`;
+const structurePrix = `<p>${data.price}</p>`;
+const structureDescription = `<p>${data.description}</p>`;
 
 
 positionImage.innerHTML = structureImage;
 positionTitre.innerHTML = structureTitre;
+positionPrix.innerHTML = structurePrix;
+positionDescription.innerHTML = structureDescription;
 })
