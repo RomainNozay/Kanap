c5f4fad7ffbc0680b9823a96b22101e75bc02359
Nozay Romain <romain.nozay@gmail.com>
2022-09-29 15:29:12 +0200

    Harmonisation de la présentation du code page index
    Code et commentaires harmonisés entre les deux pages pour meilleur présentation et compréhension.

--- front/js/script.js
+++ front/js/script.js
@@ -1,24 +1,28 @@
 
+// Apparition du catalogue des canapés sur la page principale.
 
+// I) Récupération des données de tout les canapés du Dom. (explication de fetch avec get)
 
-// Utiliser fetch pour récupérer (get par défaut) les données de l'API
-fetch("http://localhost:3000/api/products")
+// 1) Utilisation fetch pour récupérer (get par défaut) les données de l'API.
+  fetch("http://localhost:3000/api/products")
 
-// Traiter la réponse en transformant cette dernière en fichier json
+  // 2) Traitement de la réponse en la transformant en fichier json.
     .then(reponse => reponse.json())
     
-//Création d'une fonction "products"
+  // 3) Création d'une fonction "products".
         .then(function(products) {
-        
-//Utilisation de la boucle for... of pour afficher les objets dans la page
+
+  // II) Génération de contenu afin de remplir automatiquement les "card" destinés à recevoir les canapés, à partir de la fonction products qui fourni les informations.
+
+  // 1) Utilisation de la boucle for... of pour automatiser la création des "card".
         for(let product of products) {
           let i = 0; i < product.length; i++;
 
-//Attacher la structure HTML (grâce à inner.HTML) à l'id "items" (grâce à getElementById), le "+" est la pour créer un nouvel élément à chaque itération.
+  // 2) Sélectionner de la balise "items" (getElementById) sur laquelle fixer notre structure Html (grâce à inner.html). Le "+" avant le '=' est la pour créer un nouvel élément à chaque itération.
           document.getElementById("items").innerHTML += 
 
-//Création de cjaque vignette en récupérant les information nécessaire dans le Dom et en le renseignant dans les bonnes balise, elle même correctement concaténé.
-//Créer lien avec début du lien fixe et ajout de chaque produit à la fin (créer des pages uniques pour chaque produit).
+  // 3) Création de chaque vignette en récupérant les information nécessaire dans le Dom et en le renseignant dans les bonnes balise, elle même correctement concaténé.
+  // 3.1)Création du lien unique de chaque produit grâce au début fixe du lien auquel on ajoute l'id de chaque produit.
           `<a href="./product.html?id=${product._id}">
                                                           
           <article>
