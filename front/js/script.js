f6423a479f5be4b298d57f52e81982cb216051a9
Nozay Romain <romain.nozay@gmail.com>
2022-09-29 14:56:15 +0200

    Amélioration des commentaires sur le code pour présentation mentorat

--- front/js/script.js
+++ front/js/script.js
@@ -1,19 +1,24 @@
-// Utiliser fetch pour récupérer (get par défaut) les données de l'API
 
+
+
+// Utiliser fetch pour récupérer (get par défaut) les données de l'API
 fetch("http://localhost:3000/api/products")
 
 // Traiter la réponse en transformant cette dernière en fichier json
     .then(reponse => reponse.json())
     
-//Création d'une fonction products
+//Création d'une fonction "products"
         .then(function(products) {
         
 //Utilisation de la boucle for... of pour afficher les objets dans la page
         for(let product of products) {
           let i = 0; i < product.length; i++;
-//Attacher la structure HTML à l'id "items" le "+" est la pour créer nouvelle élément à chaque itération
+
+//Attacher la structure HTML (grâce à inner.HTML) à l'id "items" (grâce à getElementById), le "+" est la pour créer un nouvel élément à chaque itération.
           document.getElementById("items").innerHTML += 
-//Créer lien avec début du lien fixe et ajout de chaque produit à la fin (créer des pages uniques pour chaque produit)
+
+//Création de cjaque vignette en récupérant les information nécessaire dans le Dom et en le renseignant dans les bonnes balise, elle même correctement concaténé.
+//Créer lien avec début du lien fixe et ajout de chaque produit à la fin (créer des pages uniques pour chaque produit).
           `<a href="./product.html?id=${product._id}">
                                                           
           <article>
