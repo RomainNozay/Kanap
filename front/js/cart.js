6db325ed33d599b6b19fb6a92e7ac231e01b5c8b
Nozay Romain <romain.nozay@gmail.com>
2022-10-04 14:46:09 +0200

    Affichage du panier.
    Affichage à partir du local storage

---
+++ front/js/cart.js
@@ -0,0 +1,37 @@
+let produitEnregistre = JSON.parse(localStorage.getItem("produit"));
+console.log(produitEnregistre);
+
+const positionPanier = document.querySelector("#cart__items");
+
+let structurePanier = [];
+for( k = 0; k < produitEnregistre.length; k++){
+    structurePanier = structurePanier + 
+    `
+    <article class="cart__item" data-id="${produitEnregistre[k]._id}" data-color="${produitEnregistre[k].option_Couleur}">
+                <div class="cart__item__img">
+                  <img src="${produitEnregistre[k].image}" alt="Photographie d'un canapé">
+                </div>
+                <div class="cart__item__content">
+                  <div class="cart__item__content__description">
+                    <h2>${produitEnregistre[k].nom}</h2>
+                    <p>${produitEnregistre[k].option_Couleur}</p>
+                    <p>${produitEnregistre[k].prix} €</p>
+                  </div>
+                  <div class="cart__item__content__settings">
+                    <div class="cart__item__content__settings__quantity">
+                      <p>Qté :  </p>
+                      <input type="number" class="itemQuantity" name="${produitEnregistre[k].option_Quantite}" min="1" max="100" value="${produitEnregistre[k].option_Quantite}">
+                    </div>
+                    <div class="cart__item__content__settings__delete">
+                      <p class="deleteItem">Supprimer</p>
+                    </div>
+                  </div>
+                </div>
+              </article>
+              `
+    console.log(structurePanier);
+    if(k === produitEnregistre.lenght); {
+    positionPanier.innerHTML = structurePanier;
+    }
+}
+  
--- front/js/product.js
+++ front/js/product.js
@@ -1,124 +1,126 @@
 
 // I) Récupération de l'id dans L'url.
 
-    const queryString_url_id = window.location.search;
+const queryString_url_id = window.location.search;
 
-    const urlSearchParams = new URLSearchParams(queryString_url_id);
+const urlSearchParams = new URLSearchParams(queryString_url_id);
 
-    const id = urlSearchParams.get("id");
+const id = urlSearchParams.get("id");
 
 
 // II) Extraction des informations d'un seul produit pour remplir la page.
- 
-    fetch(`http://localhost:3000/api/products/${id}`)
-    .then(function (response) {
-    return response.json()
-    })
-    .then(function (informations) {
-        informationProduit(informations);
-        selectionProduit(informations);
-    console.log(informations);
-    })
+
+fetch(`http://localhost:3000/api/products/${id}`)
+.then(function (response) {
+return response.json()
+})
+.then(function (informations) {
+    informationProduit(informations);
+    selectionProduit(informations);
+console.log(informations);
+})
 
 
-    //Gestion des données pour afficher les éléments sur la page
+//Gestion des données pour afficher les éléments sur la page
 
-    let informationProduit = (informations) => {
+let informationProduit = (informations) => {
 
-    const positionImage = document.querySelector(".item__img ");
-    const positionTitre = document.querySelector("#title");
-    const positionPrix = document.querySelector("#price");
-    const positionDescription = document.querySelector("#description");
-    const positionTitrePage = document.querySelector("title");
-    const positionCouleur = document.querySelector("#colors");
+const positionImage = document.querySelector(".item__img ");
+const positionTitre = document.querySelector("#title");
+const positionPrix = document.querySelector("#price");
+const positionDescription = document.querySelector("#description");
+const positionTitrePage = document.querySelector("title");
+const positionCouleur = document.querySelector("#colors");
 
-    const structureImage = `<img src=${informations.imageUrl}> `;
-    const structureTitre = `<h1>${informations.name}</h1>`;
-    const structurePrix = `${informations.price}`;
-    const structureDescription = `<p>${informations.description}</p>`;
-    const structureTitrePage = `${informations.name}`;
+const structureImage = `<img src=${informations.imageUrl}> alt="${informations.altTxt}" `;
+const structureTitre = `<h1>${informations.name}</h1>`;
+const structurePrix = `${informations.price}`;
+const structureDescription = `<p>${informations.description}</p>`;
+const structureTitrePage = `${informations.name}`;
 
-    let structureCouleur = [];
-    for (i = 0; i < informations.colors.length; i++) {
-        structureCouleur = structureCouleur +
-        `<option value="${informations.colors[i]}">${informations.colors[i]}</option>`;
-    }
+let structureCouleur = [];
+for (i = 0; i < informations.colors.length; i++) {
+    structureCouleur = structureCouleur +
+    `<option value="${informations.colors[i]}">${informations.colors[i]}</option>`;
+}
 
-    positionImage.innerHTML = structureImage;
-    positionTitre.innerHTML = structureTitre;
-    positionPrix.innerText = structurePrix;
-    positionDescription.innerHTML = structureDescription;
-    positionTitrePage.innerText = structureTitrePage;
-    positionCouleur.innerHTML += structureCouleur;
-    }
+positionImage.innerHTML = structureImage;
+positionTitre.innerHTML = structureTitre;
+positionPrix.innerText = structurePrix;
+positionDescription.innerHTML = structureDescription;
+positionTitrePage.innerText = structureTitrePage;
+positionCouleur.innerHTML += structureCouleur;
+}
 
 
 
 // III) Récupération des données sélectionnées par l'utilisateur.
 
-    const menuCouleur = document.querySelector("#colors");
-    const menuQuantite = document.querySelector("#quantity");
-    
-    const boutonPanier = document.querySelector("#addToCart");
-    console.log(boutonPanier);
+const menuCouleur = document.querySelector("#colors");
+const menuQuantite = document.querySelector("#quantity");
 
-    let selectionProduit = (informations) => {
-    boutonPanier.addEventListener("click", (event)=>{
-    event.preventDefault();  
+const boutonPanier = document.querySelector("#addToCart");
+console.log(boutonPanier);
 
-    const choixCouleur = menuCouleur.value;
-    const choixQuantite = menuQuantite.value;
+let selectionProduit = (informations) => {
+boutonPanier.addEventListener("click", (event)=>{
+event.preventDefault();  
 
-    if (menuCouleur.value == false) {
-        (window.alert (`Veuillez sélectionner une couleur pour votre ${informations.name}`));
-        window.location.href = "#colors";
-      } else if (menuQuantite.value == 0) {
-        (window.alert (` Veuillez sélectionner le nombre de ${informations.name} souhaités`));
-        window.location.href = "#quantity";
-      } else if (window.confirm (` Nous avons bien ajouté ${menuQuantite.value} ${informations.name}, couleur: "${menuCouleur.value}" à votre panier.
-      Consultez le panier: OK ou Continuer votre shopping: ANNULER`)){
-        window.location.href = "cart.html";
-      }else{
-        
-      }
-      
-    
+const choixCouleur = menuCouleur.value;
+const choixQuantite = menuQuantite.value;
 
-    let ChoixUtilisateur = {
-    _id: id,
-    image: informations.imageUrl,
-    option_Couleur: choixCouleur,
-    option_Quantite: choixQuantite,
-    prix: informations.price,
-    nom: informations.name,
-    }
-    console.log(ChoixUtilisateur);
+if (menuCouleur.value == false) {
+    (window.alert (`Veuillez sélectionner une couleur pour votre ${informations.name}`));
+    window.location.href = "#colors";
+  } else if (menuQuantite.value == 0) {
+    (window.alert (` Veuillez sélectionner le nombre de ${informations.name} souhaités`));
+    window.location.href = "#quantity";
+  } else if (window.confirm (` Nous avons bien ajouté ${menuQuantite.value} ${informations.name}, couleur: "${menuCouleur.value}" à votre panier.
+  Consultez le panier: OK ou Continuer votre shopping: ANNULER`)){
+    window.location.href = "cart.html";
+  }else{
     
-    
+  }
+  
+
+
+let ChoixUtilisateur = {
+_id: id,
+image: informations.imageUrl,
+option_Couleur: choixCouleur,
+option_Quantite: choixQuantite,
+prix: informations.price,
+nom: informations.name,
+}
+console.log(ChoixUtilisateur);
+
+
 
 // Ajouter au local storage
 
-    let produitEnregistre = JSON.parse(localStorage.getItem("produit"));
+let produitEnregistre = JSON.parse(localStorage.getItem("produit"));
+console.log(produitEnregistre);
+
+if(produitEnregistre === null){
+    produitEnregistre = [];
+    produitEnregistre.push(ChoixUtilisateur);
+    localStorage.setItem("produit", JSON.stringify(produitEnregistre));
     console.log(produitEnregistre);
-
-    if(produitEnregistre === null){
-        produitEnregistre = [];
-        produitEnregistre.push(ChoixUtilisateur);
+}
+else{
+    const found = produitEnregistre.find(element => element._id == ChoixUtilisateur._id && element.option_Couleur == ChoixUtilisateur.option_Couleur);
+    if (found == undefined) {
+        console.log(found)
+    produitEnregistre.push(ChoixUtilisateur);
+    localStorage.setItem("produit", JSON.stringify(produitEnregistre));
+    }else{
+        found.option_Quantite +=  ChoixUtilisateur.option_Quantite;
+        console.log(produitEnregistre.option_Quantite);
+        console.log(found.option_Quantite);
+        console.log(ChoixUtilisateur.option_Quantite)
         localStorage.setItem("produit", JSON.stringify(produitEnregistre));
-        console.log(produitEnregistre);
     }
-    else{
-        const found = produitEnregistre.find(element => element._id == ChoixUtilisateur._id && element.option_Couleur == ChoixUtilisateur.option_Couleur);
-        console.log(found);
-        if (found == undefined) {
-            console.log(found)
-        produitEnregistre.push(ChoixUtilisateur);
-        localStorage.setItem("produit", JSON.stringify(produitEnregistre));
-        }else{
-            element.option_Quantite = produitEnregistre.option_Quantite + ChoixUtilisateur.option_Quantite;
-            localStorage.setItem("produit", JSON.stringify(produitEnregistre));
-        }
-    }
+}
 
-    })
-    }
+})
+}
