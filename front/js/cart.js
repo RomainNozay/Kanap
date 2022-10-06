9284d5ff7c55b6496a870704e89a70aadf3470d4
Nozay Romain <romain.nozay@gmail.com>
2022-10-06 12:22:01 +0200

    ajout de la fonction ajout quantité
    mais avec un bémol cela ne fonctionne qu'avec le 1er élément.  On continu comme ça.

--- front/js/cart.js
+++ front/js/cart.js
@@ -35,18 +35,39 @@ for( k = 0; k < produitEnregistre.length; k++){
     }
 }
 
+
 //function changeQuantite(){
-
-  const bouttonQuantitePanier = document.querySelectorAll("input.itemQuantity");
-  console.log(bouttonQuantitePanier);
-  for (i = 0; i < bouttonQuantitePanier.length; i++){
-    bouttonQuantitePanier[i].addEventListener("change", change);
-    function change(){
-    console.log("coucou");
-
-  }
-}
-
+  
+  //const bouttonQuantitePanier = document.querySelectorAll("input.itemQuantity");
+  //console.log(bouttonQuantitePanier);
+  //for (i = 0; i < bouttonQuantitePanier.length; i++){
+    //const bouttonQuantitePanierValeur = bouttonQuantitePanier.value;
+      //console.log(bouttonQuantitePanier.value);
+    //bouttonQuantitePanier[i].addEventListener("change", change);
+    //function change() {
+      
+ // Modification d'une quantité de produit
+ let bouttonQuantitePanier = document.querySelectorAll(".itemQuantity");
+ for (i = 0; i < bouttonQuantitePanier.length; i++){
+   console.log(bouttonQuantitePanier[i]);
+ const choixQuantiteEnregistre = bouttonQuantitePanier[i].value;
+  console.log(choixQuantiteEnregistre);
+ 
+  bouttonQuantitePanier[i].addEventListener("change", nouvelleValeur);
+  function nouvelleValeur() {  
+   for (i = 0; i < bouttonQuantitePanier.length; i++){
+ let nouveauChoix = bouttonQuantitePanier[i].value;
+  console.log("nouvelle quantité rensigné =", nouveauChoix);
+  const test = produitEnregistre.find(el => el.option_Quantite !== nouveauChoix);
+  test.option_Quantite = nouveauChoix;
+  console.log("nouvelle quantite dans test.option_Quantite =", test.option_Quantite);
+  localStorage.setItem("produit", JSON.stringify(produitEnregistre));
+  console.log(produitEnregistre);
+  //location.reload();
+ 
+ } 
+ }
+ }
 
 
 
